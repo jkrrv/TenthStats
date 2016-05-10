f8ed03fbbe6f7eff62b77dacccc05c8aa4e3c7fa
James Kurtz <jkrrv@jkrrv.org>
2016-05-10 12:34:50 -0400

    More classes; more functions within existing classes

--- _db.php
+++ _db.php
@@ -88,19 +88,26 @@ abstract class Base {
 
 		global $pdo;
 
+		$pdo->beginTransaction();
 		if ($this->id == 0) {
-			$pdo->beginTransaction();
 			$s = $pdo->prepare($this->getInsertStatement());
-			$this->id = (int) $pdo->lastInsertId();
-			$pdo->commit();
 		} else {
 			$s = $pdo->prepare($this->getUpdateStatement());
 		}
 		foreach ($this->getMemberNames() as $n) {
 			$s->bindValue(":" . $n, $this->$n);
 		}
+
+		$ex = $s->execute();
+
+		if ($this->id == 0) {
+			$this->id = (int) $pdo->lastInsertId();
+		}
+
+		$pdo->commit();
 		$this->_dirty = false;
-		return $s->execute();
+
+		return $ex;
 	}
 
 	public function __get($what) {
@@ -148,8 +155,9 @@ class Grp extends Base {
 	protected $id;
 	protected $name;
 	protected $type;
+	protected $gender = 0;
 
-	protected $tableName = "_grps";
+	protected $_tableName = "_grps";
 
 	public function stringSearch($string) {
 		global $pdo;
@@ -163,6 +171,146 @@ class Grp extends Base {
 		} catch (Exception $e) {
 			return 0;
 		}
+	}
+}
+
+
+class GrpMem extends Base {
+	protected $id;
+	protected $person;
+	protected $grp;
+	protected $level;
+	protected $dJoin;
+	protected $dLeft;
+
+	protected $_tableName = "_grpmem";
+
+	public function stringSearch($string) {
+		global $pdo;
+
+		if (is_numeric($string)) {
+			$string = explode(".", $string);
+			$s = $pdo->prepare("SELECT id FROM _grpmem WHERE person = :p AND grp = :g");
+			try {
+				$s->execute([
+					':p' => (int)$string[0],
+					':g' => (int)$string[1]
+				]);
+				return $s->fetchColumn(0);
+			} catch (Exception $e) {
+				return 0;
+			}
+		} else {
+
+			throw new Exception("oops");
+
+		}
+	}
+}
+
+
+class Event extends Base {
+	protected $id;
+	protected $grp;
+	protected $dt;
+	protected $location;
+	protected $facebookId;
+	protected $name;
+
+	protected $_tableName = "_events";
+
+	public function stringSearch($string) {
+		global $pdo;
+
+		if (strpos($string,"f/") === 0) {
+			$s = $pdo->prepare("SELECT id FROM _events WHERE facebookId = :facebookId");
+			try {
+				$s->execute([
+					':facebookId' => substr($string,2)
+				]);
+				return $s->fetchColumn(0);
+			} catch (Exception $e) {
+				return 0;
+			}
+		} else {
+			$s = $pdo->prepare("SELECT id FROM _events WHERE name = :name");
+			try {
+				$s->execute([
+					':name' => $string
+				]);
+				if ($s->rowCount() > 1) {
+					throw new Exception("Event Name is Ambiguous.");
+				}
+				return $s->fetchColumn(0);
+			} catch (Exception $e) {
+				return 0;
+			}
+		}
+	}
+
+	public function __toString() {
+		return "" . $this->name;
+	}
+}
+
+
+class Opp extends Base {
+	protected $id;
+	protected $person;
+	protected $event;
+	protected $status;
+	protected $confidence;
+	protected $source;
+
+	protected $_tableName = '_opportunities';
+
+	public function __construct($event, $person, $source = null) {
+		global $pdo;
+
+		$id = 0;
+
+		if ($person === null) {
+			$id = $event;
+		} else if ($source != null) {
+			$s = $pdo->prepare("SELECT id FROM _opportunities WHERE event = :event AND person = :person AND source = :source");
+			try {
+				$s->execute([
+					':event' => (int)$event,
+					':person' => (int)$person,
+					':source' => $source
+				]);
+				$id = $s->fetchColumn(0);
+			} catch (Exception $e) {
+				$id = 0;
+			}
+		} else if ($id == 0) {
+			$s = $pdo->prepare("SELECT id FROM _opportunities WHERE event = :event AND person = :person");
+			try {
+				$s->execute([
+					':event' => (int)$event,
+					':person' => (int)$person
+				]);
+				$id = $s->fetchColumn(0);
+			} catch (Exception $e) {
+				$id = 0;
+			}
+		}
+
+		if ($id == 0) {
+			$id = ' ';
+		}
+
+		parent::__construct($id);
+	}
+
+	public function stringSearch($string) {
+		if ($string === ' ')
+			return 0;
+		throw new Exception("Unexpected constructor arguments.");
+	}
+
+	public function getEvent() {
+		return new Event($this->event);
 	}
 }
 
@@ -184,6 +332,7 @@ class Person extends Base {
 	protected $dBirth;
 	protected $dJoined;
 	protected $parish;
+	protected $facebookId;
 	
 
 	protected $_tableName = "_people";
@@ -204,21 +353,67 @@ class Person extends Base {
 				return 0;
 			}
 		} else {
+			$st = explode(",", $string);
 
-			throw new Exception("oops");
+			if (count($st) != 2)
+				$st = explode(" ", $string);
 
-			global $pdo;
+			if (count($st) != 2)
+				throw new Exception('unexpected number of names : ' . $string);
 
-			$s = $pdo->prepare("SELECT id FROM _people WHERE address = :str OR name = :str");
+			$s = $pdo->prepare("SELECT id FROM _people WHERE upper(lName) = upper(:lName) AND (upper(fName) = upper(:fName) OR upper(pName) = upper(:fName))");
 			try {
-				$s->execute([':str' => $string]);
-				return $s->fetchColumn(0);
+				$s->execute([
+					':fName' => $st[0],
+					':lName' => $st[1]
+				]);
 			} catch (Exception $e) {
 				return 0;
 			}
 
+			if ($s->rowCount() > 1) {
+				throw new Exception("Ambiguous Records : matches multiple people");
+			}
+			return $s->fetchColumn(0);
 
 		}
+	}
+
+	public function getPrefName() {
+		if ($this->pName != null)
+			return $this->pName;
+		return $this->fName;
+	}
+
+	public function __toString() {
+		return $this->getPrefName() . " " . $this->lName;
+	}
+
+
+	public function getOppsTaken() {
+		global $pdo;
+
+		$opps = [];
+
+		$s = $pdo->prepare("
+			SELECT o.id 
+			FROM _opportunities AS o 
+			  JOIN _events AS e
+			  	ON o.event = e.id
+		 	WHERE person = :person AND 
+		 		status > 0 
+			ORDER BY e.dt DESC, o.confidence DESC");
+		$s->execute([
+			':person' => $this->id
+		]);
+
+
+		while (($o = $s->fetchColumn(0)) !== false) {
+			$opps[] = new Opp($o, null);
+		}
+
+		return $opps;
+
 	}
 }
 
